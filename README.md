fd35e6d8f87eff5bdba993fefcf6b86e67a505f5
AntonBoreiko <AntonBoreiko>
2021-08-06 10:37:03 +0300

    Change css

--- README.md
+++
@@ -1,7 +0,0 @@
-# Bolierplate parcel.js
-
-
-1. `git clone https://github.com/AntonBoreiko/bolierplate-parcel.git`
-2. `cd ./bolierplate-parcel`
-3. `npm install`
-4. `npm run dev `
--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,1 +1,18 @@
-console.info('HEllo')
+
+const burger = document.querySelector('.header__burger')
+const mobile = document.querySelector('.mobile')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active')
+  mobile.classList.toggle('active')
+})
+
+
+const myCarousel = document.querySelector('#myCarousel')
+const carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 1800,
+  wrap: true,
+  pause: 'hover',
+  ride: true,
+  touch: true,
+})
